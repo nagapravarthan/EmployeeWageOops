ad15e64f29c12c860c2c32caa614883521f884be
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 09:07:44 +0530

    EmployeeWageOops

---
+++ employeewage/EmployeeWage.java
@@ -0,0 +1,25 @@
+package employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				checkAttendance employee = new checkAttendance();
+				employee.isPresent();
+			    }
+			}
+
+			class checkAttendance {
+				//Constants
+				int IS_FULL_TIME = 1;
+				//Computation
+			    	double empCheck = Math.floor(Math.random() * 10) % 2;
+			    	void isPresent(){
+			        if ( empCheck == IS_FULL_TIME )
+			            System.out.println("Employee is Present !");
+			        else
+			            System.out.println("Employee is Absent !");
+			    }
+
+}
