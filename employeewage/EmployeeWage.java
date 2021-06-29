39ca616dc913a970541b5650c9ee6b4e4084a8a8
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 09:31:17 +0530

    uc1present_or_absent

--- employeewage/EmployeeWage.java
+++ employeewage/EmployeeWage.java
@@ -2,33 +2,21 @@ package employeewage;
 
 public class EmployeeWage {
 	public static void main(String[] args){
-		checkAttendance employee = new checkAttendance();
-		employee.isPresent();
-	    }
+	
+			checkAttendance employee = new checkAttendance();
+			employee.isPresent();
+		    }
+		}
+
+		class checkAttendance {
+			//Constants
+			int IS_FULL_TIME = 1;
+			//Computation
+		    	double empCheck = Math.floor(Math.random() * 10) % 2;
+		    	void isPresent(){
+		        if ( empCheck == IS_FULL_TIME )
+		            System.out.println("Employee is Present !");
+		        else
+		            System.out.println("Employee is Absent !");
+		    }
 	}
-
-class checkAttendance extends calculateWage{
-	//Computation
-    	double empCheck = Math.floor(Math.random() * 10) % 3;
-        void isPresent(){
-        int check = 0;
-		if ( check == 0 ) {
-            System.out.println("Employee is Present for full time !");
-            System.out.println("Wage is "+dailyWage(8));
-        }
-        else if ( check == 1 ) {
-            System.out.println("Employee is Present for part time !");
-            System.out.println("Wage is "+dailyWage(4));
-        }
-        else
-            System.out.println("Employee is Absent !");
-}
-}
-
-//Wage computation 
-class calculateWage {
-    	final int wagePerHr = 20;
-    	int dailyWage(int workHr){
-    		return wagePerHr * workHr;
-        }
-    }
