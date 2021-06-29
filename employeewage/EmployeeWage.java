0bfed60c82a0780e5ac2b1c65c86348ba9e30608
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 10:21:05 +0530

    uc6conditional_wage

--- employeewage/EmployeeWage.java
+++ employeewage/EmployeeWage.java
@@ -10,30 +10,36 @@ public class EmployeeWage {
 class checkAttendance extends calculateWage{
 	//Computation
 	int check;
-    	static int days = 0;
-    void isPresent(){
-        int wage = 0;
-        int totalWage = 0;
-        while ( days != 20 ){
-            days++;
-            check = (int)(Math.floor(Math.random() * 10) % 3);
-            switch(check) {
-                case 0:
-                    wage = dailyWage(8);
-                    break;
-                case 1:
-                    wage = dailyWage(4);
-                    break;
-                default:
-                    wage = 0;
-                    break;
-            }
-            totalWage = totalWage + wage;
+	static int days = 0;
+	static int workHrs = 0;
+	static int totalWorkHrs = 0;
+void isPresent(){
+    int wage = 0;
+    int totalWage = 0;
+    while ( days != 20 && totalWorkHrs <= 100 ) {
+        days++;
+        System.out.println(days);
+        check = (int)(Math.floor(Math.random() * 10) % 3);
+        switch(check) {
+            case 0:
+                workHrs = 8;
+                break;
+            case 1:
+                workHrs = 4;
+                break;
+            default:
+                workHrs = 0;
+                break;
         }
-        System.out.println("Total earning of employee for a month is "+totalWage);
+        totalWorkHrs = totalWorkHrs + workHrs;
+        wage = dailyWage(workHrs);
+        totalWage = totalWage + wage;
     }
+    System.out.println("Total earning of employee for a month is "+totalWage);
 }
-
+}
+            
+            
 //Wage computation 
 class calculateWage {
     	final int wagePerHr = 20;
