767db73c82dd40afe17de7aa207421d0c52613c1
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 09:18:21 +0530

    uc2Employee_daily_wage

--- employeewage/EmployeeWage.java
+++ employeewage/EmployeeWage.java
@@ -1,25 +1,30 @@
 package employeewage;
 
 public class EmployeeWage {
+	public static void main(String[] args){
+		checkAttendance employee = new checkAttendance();
+		employee.isPresent();
+	    }
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class checkAttendance extends calculateWage{
+	//Constants
+	int IS_FULL_TIME = 1;
+	//Computation
+    	double empCheck = Math.floor(Math.random() * 10) % 2;
+    	void isPresent(){
+        if ( empCheck == IS_FULL_TIME )
+            System.out.println("Employee is Present !");
+	    System.out.println("Wage is "+dailyWage());	
+            System.out.println("Employee is Absent !");
+    }
+}
 
-				checkAttendance employee = new checkAttendance();
-				employee.isPresent();
-			    }
-			}
-
-			class checkAttendance {
-				//Constants
-				int IS_FULL_TIME = 1;
-				//Computation
-			    	double empCheck = Math.floor(Math.random() * 10) % 2;
-			    	void isPresent(){
-			        if ( empCheck == IS_FULL_TIME )
-			            System.out.println("Employee is Present !");
-			        else
-			            System.out.println("Employee is Absent !");
-			    }
-
+//Wage computation 
+class calculateWage {
+    	final int wagePerHr = 20;
+    	final int workHr = 8;
+    	int dailyWage(){
+        return wagePerHr * workHr;
+    }
 }
