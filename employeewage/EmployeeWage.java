dc83b568ef0929c8b1c9cfe5d56952b2473b674e
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 09:53:30 +0530

    uc4switchcase

--- employeewage/EmployeeWage.java
+++ employeewage/EmployeeWage.java
@@ -2,21 +2,36 @@ package employeewage;
 
 public class EmployeeWage {
 	public static void main(String[] args){
-	
-			checkAttendance employee = new checkAttendance();
-			employee.isPresent();
-		    }
-		}
-
-		class checkAttendance {
-			//Constants
-			int IS_FULL_TIME = 1;
-			//Computation
-		    	double empCheck = Math.floor(Math.random() * 10) % 2;
-		    	void isPresent(){
-		        if ( empCheck == IS_FULL_TIME )
-		            System.out.println("Employee is Present !");
-		        else
-		            System.out.println("Employee is Absent !");
-		    }
+		checkAttendance employee = new checkAttendance();
+		employee.isPresent();
+	    }
 	}
+
+class checkAttendance extends calculateWage{
+	//Computation
+    	double empCheck = Math.floor(Math.random() * 10) % 3;
+        void isPresent(){
+        int check = 0;
+		switch(check) {
+            case 0:
+                System.out.println("Employee is Present for full time !");
+                System.out.println("Wage is " + dailyWage(8));
+                break;
+            case 1:
+                System.out.println("Employee is Present for part time !");
+                System.out.println("Wage is " + dailyWage(4));
+                break;
+            default:
+                System.out.println("Employee is Absent !");
+                break;
+        }
+    } 
+}
+
+//Wage computation 
+class calculateWage {
+    	final int wagePerHr = 20;
+    	int dailyWage(int workHr){
+    		return wagePerHr * workHr;
+        }
+    }
