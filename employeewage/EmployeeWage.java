b7fc7f875785b6ac1c3df283805980a0a38b2e41
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-29 09:24:50 +0530

    uc3part_time

--- employeewage/EmployeeWage.java
+++ employeewage/EmployeeWage.java
@@ -8,23 +8,27 @@ public class EmployeeWage {
 	}
 
 class checkAttendance extends calculateWage{
-	//Constants
-	int IS_FULL_TIME = 1;
 	//Computation
-    	double empCheck = Math.floor(Math.random() * 10) % 2;
-    	void isPresent(){
-        if ( empCheck == IS_FULL_TIME )
-            System.out.println("Employee is Present !");
-	    System.out.println("Wage is "+dailyWage());	
+    	double empCheck = Math.floor(Math.random() * 10) % 3;
+        void isPresent(){
+        int check = 0;
+		if ( check == 0 ) {
+            System.out.println("Employee is Present for full time !");
+            System.out.println("Wage is "+dailyWage(8));
+        }
+        else if ( check == 1 ) {
+            System.out.println("Employee is Present for part time !");
+            System.out.println("Wage is "+dailyWage(4));
+        }
+        else
             System.out.println("Employee is Absent !");
-    }
+}
 }
 
 //Wage computation 
 class calculateWage {
     	final int wagePerHr = 20;
-    	final int workHr = 8;
-    	int dailyWage(){
-        return wagePerHr * workHr;
+    	int dailyWage(int workHr){
+    		return wagePerHr * workHr;
+        }
     }
-}
